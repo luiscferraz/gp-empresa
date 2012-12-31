38dcc07f8b7bff47cc57d1898e797e95283c7570
palaciowagner <palaciowagner@c736e0bd-485d-982a-d027-c4d9b3a031cf>
2012-12-31 01:55:06 +0000

    Inclusão do CompaniesController

--- trunk/Project/app/Controller/CompaniesController.php
+++ trunk/Project/app/Controller/CompaniesController.php
@@ -3,10 +3,25 @@
 
 class CompaniesController extends AppController {
 	
-	public $uses = 'Company'
-	public $name 'Companies'
+	public $uses = 'Company';
+	public $name = 'Companies';
 	
 	public function index(){
+		$this->set('companies', $this->Company->find('all'));		
+	}
+	
+	public function add(){
+		
+		if($this->request->is('post')){
+			if($this->Company->saveAll($this->request->data)){
+				$this->Session->setFlash('Empresa cadastrada com sucesso!');
+				$this->redirect(array('action' => 'index'));
+				 
+			}
+		}
+		else{			
+			$this->Session->setFlash('Empresa não cadastrada. Tente novamente!');			
+		}
 		
 	}
 	
