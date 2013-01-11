a1d5dfa6b8e1d4fd166e088f9f27126bd206cf8e
allandoamaral@gmail.com <allandoamaral@gmail.com@c736e0bd-485d-982a-d027-c4d9b3a031cf>
2013-01-11 05:28:27 +0000

    Inclusão View Add

--- trunk/Project/app/Controller/ProjectsController.php
+++ trunk/Project/app/Controller/ProjectsController.php
@@ -9,4 +9,20 @@ class ProjectsController extends AppController {
 		$this->set('projects', $this->paginate());
 	}
 
+	public function add(){
+
+		$this -> layout = 'AddProject';
+
+		if($this->request->is('post')){
+			if($this->Project->saveAll($this->request->data)){
+				$this->Session->setFlash('Projeto cadastrado com sucesso!');
+				$this->redirect(array('action' => 'index'));
+				 
+			}
+		}
+		else{			
+			$this->Session->setFlash('O projeto não foi cadastrado. Tente novamente!');			
+		}
+		
+	}
 }
