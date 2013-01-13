32eafbe472ed57050a80b9de425cc077e875679a
palaciowagner <palaciowagner@c736e0bd-485d-982a-d027-c4d9b3a031cf>
2013-01-13 23:15:14 +0000

    Alterações no add e existe no ProjectsController

--- trunk/Project/app/Model/Project.php
+++ trunk/Project/app/Model/Project.php
@@ -2,13 +2,15 @@
 App::uses('AppModel', 'Model');
 
 class Project extends AppModel {
+	
+		public $useTable = 'projects';
         public $name = 'Project';
         public $belongsTo = array(
                 'Company'=> array(
-                        'className' => 'Company',
-                 'fields' => array('id', 'name'),
-                 'conditions' => array(),        
-                 'dependent' => true
+            				'className' => 'Company',
+                 			'fields' => array('id', 'name'),
+                 			'conditions' => array(),        
+                			'dependent' => true
                 ));
        
  }
