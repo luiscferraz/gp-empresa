dafffd80c8eaee15632ec6b58434fa8b4d59939b
palaciowagner <palaciowagner@c736e0bd-485d-982a-d027-c4d9b3a031cf>
2013-01-13 23:15:14 +0000

    Alterações no add e existe no ProjectsController

--- Project/app/Controller/ProjectsController.php
+++ Project/app/Controller/ProjectsController.php
@@ -1,38 +1,61 @@
 ﻿<?php
 App::uses('AppController', 'Controller');
 
-class ProjectsController extends AppController {
-        public $helpers = array ('html','form');
-        public $name = 'Projects';
-        var $scaffold;
 
-
+class ProjectsController extends AppController{
+	
+	public $helpers = array ('html','form');
+   	public $uses = 'Project';
+   	public $name = 'Projects';
+    var $scaffold;
+    
+	
+    
 	public function index(){
+
+
 		$this -> layout = 'IndexProject';
 		$this->set('projects', $this->Project->find('all', array('conditions'=> array('Project.inactive !=' => 1))));		
+
 	}
-
+	
 	public function add(){
-
+		
 		$this -> layout = 'AddProject';
 
 		if($this->request->is('post')){
-			if($this->Project->saveAll($this->request->data)){
-				$this->Session->setFlash('Projeto cadastrado com sucesso!');
-				$this->redirect(array('action' => 'index'));
+			#$this->Project->set($this->request->data);
+			#echo $this->existe($this->request->data['Project']['name'],$this->request->data['Project']['company_id']);
+			if ($this->existe($this->request->data['Project']['name'],$this->request->data['Project']['company_id']) == 0){
+							
+				if($this->Project->saveAll($this->request->data)){
+					$this->Session->setFlash('Projeto cadastrado com sucesso!');
+					$this->redirect(array('action' => 'index'));
+				}
+				else{			
+					$this->Session->setFlash('O projeto nao foi cadastrado! Tente novamente');			
+				}
+			}
+			else{
+				$this->Session->setFlash('Projeto ja existe no banco de dados. Favor tentar novamente.');
+				$this->redirect(array('action'=>'index'));
+			}
 				}
 				 
-			}
-		else{
-                        $this-> set ('projects',$this->Project->find('all', array('conditions'=> array('Project.inactive !=' => 1))));
-                        $this-> set ('companies',$this->Project->Company->find('all', array('conditions'=> array('Company.removed !=' => 1))));			
-		}
-		
-	}
 
-	private function existe($nome){
-		$foundProject = $this->Project->find('first',array('conditions'=> array('Project.name ==' => $nome)));
-		if ($foundProject == NULL){
+		else{
+                 $this-> set ('projects',$this->Project->find('all', array('conditions'=> array('Project.inactive !=' => 1))));
+                        
+               	 $this-> set ('companies',$this->Project->Company->find('all', array('conditions'=> array('Company.removed !=' => 1))));			
+		}
+
+			
+	}
+	
+	private function existe($nome, $idEmpresa){
+		$foundProject = $this->Project->find('first',array('conditions'=> array('Project.name =' => $nome,'Project.company_id =' => $idEmpresa)));
+		#echo count($foundProject);
+		if (count($foundProject) == 0){
 			return 0;
 		}
 		
@@ -40,7 +63,6 @@ class ProjectsController extends AppController {
 			return 1;
 		}
 	}
-
 
 	public function edit($id = NULL){
 	
@@ -50,6 +72,7 @@ class ProjectsController extends AppController {
 			throw new NotFoundException(__('Invalid post'));
 		}
 		$proj = $this->Project->findById($id);	
+		
 		if (!$proj) {
 			throw new NotFoundException(__('Invalid post'));
 		}
@@ -57,33 +80,30 @@ class ProjectsController extends AppController {
 		if ($this->request->is('get')) {
 			$this->request->data = $this->Project->read();
 			$this-> set ('projects',$this->Project->find('all'));
-                        $this-> set ('companies',$this->Project->Company->find('all'));
+            $this-> set ('companies',$this->Project->Company->find('all'));
 		}
+
 		else {
 			$this->Project->id = $id;
+		}
 		if ($this->Project->saveAll($this->request->data)) {	
 			$this->Session->setFlash('Projeto atualizado!');
 			$this->redirect(array('action' => 'index'));
-			}
 		}
+		
 	}
-
-
-
-
+	
+	
 	public function delete($id = NULL){
 		
-		$this->Project->id = $id;
-				
-		
+		$this->Project->id = $id;		
 		$this->Project->saveField('inactive',1);
-		
 		$this->Session->setFlash('Projeto removido com sucesso!');
-		
 		$this->redirect('/Projects');
 		
 	}
 
+	
 
 	public function view($id){
 
@@ -91,7 +111,8 @@ class ProjectsController extends AppController {
 		$this->layout = 'ViewProject';
 		
 	    if ($this->request->is('get')) {
-	        $this->set('project', $this->Project->read());
+	        $this->set('Project', $this->Project->read());
 	    }
 	}
 }
+?>
